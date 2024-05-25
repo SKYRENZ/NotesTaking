078d3633d8710ef1b7be288e349da2c3203314a0
insrno <christian.s3rrano06@gmail.com>
2024-05-26 02:38:57 +0800

    notepopupwindowbutton.xaml notecontrol.xaml minimize and close button icon main and dash

--- NotesTaking/MVVM/View/RemindersControl.xaml.cs
+++ NotesTaking/MVVM/View/RemindersControl.xaml.cs
@@ -9,20 +9,39 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace NotesTaking.MVVM.View
 {
-    /// <summary>
-    /// Interaction logic for RemindersControl.xaml
-    /// </summary>
     public partial class RemindersControl : UserControl
     {
         public RemindersControl()
         {
             InitializeComponent();
+        }
+
+        private void AddReminder_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(NewReminderTextBox.Text))
+            {
+                var newReminder = new Border
+                {
+                    Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3B3B3D")),
+                    CornerRadius = new CornerRadius(10),
+                    Padding = new Thickness(10),
+                    Margin = new Thickness(0, 5, 0, 5),
+                    Child = new TextBlock
+                    {
+                        Text = NewReminderTextBox.Text,
+                        Foreground = new SolidColorBrush(Colors.WhiteSmoke)
+                    }
+                };
+
+                ReminderList.Children.Add(newReminder);
+                NewReminderTextBox.Clear();
+            }
         }
     }
 }
